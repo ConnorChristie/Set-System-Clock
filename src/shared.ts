1c4f7e9a8c4d0e43b22f7aed5217411141d98800
Inrix <Inrixia@gmail.com>
2022-10-05 16:42:57 +1300

    Added function to fetch date format on windows, resolves #3
    This fixes the issue https://github.com/ConnorChristie/Set-System-Clock/issues/3

--- src/shared.ts
+++ src/shared.ts
@@ -1,3 +1,15 @@
-import { exec as _ } from "child_process";
+import { exec as execCallback } from "child_process";
 import { promisify } from "util";
-export const exec = promisify(_);
+export const exec = promisify(execCallback);
+
+export const getDateFormat = new Promise<string>((res) => {
+	let s = "";
+	const console = execCallback("date");
+	console.stdout?.on("data", (str: string) => {
+		s += str;
+		if (s.length === 69) {
+			console.kill();
+			res(s.slice(59, -2));
+		}
+	});
+});
--- src/windows.clock.ts
+++ src/windows.clock.ts
@@ -1,7 +1,7 @@
-import { exec } from "./shared";
+import { exec, getDateFormat } from "./shared";
 import * as dateFormat from "dateformat";
 
 export default async (dateTime: Date) => {
-	await exec(`date ${dateFormat(dateTime, "m/d/yyyy")}`);
+	await exec(`date ${dateFormat(dateTime, await getDateFormat)}`);
 	await exec(`time ${dateFormat(dateTime, "HH:MM:ss")}`);
 };
