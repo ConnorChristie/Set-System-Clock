be9bcbf42d0762cd4a9f10efee1aa41bfa6931d2
Inrix <Inrixia@gmail.com>
2022-10-05 17:13:36 +1300

    Fixed options not being optional for linux

--- src/linux.clock.ts
+++ src/linux.clock.ts
@@ -1,8 +1,8 @@
 import { exec } from "./shared";
 import * as dateFormat from "dateformat";
 
-export default async (dateTime: Date, options: { useSudo?: boolean }) => {
-	const sudo = options.useSudo ? "sudo" : "";
+export default async (dateTime: Date, options?: { useSudo?: boolean }) => {
+	const sudo = options?.useSudo ? "sudo" : "";
 
 	await exec(`${sudo} timedatectl set-ntp false`);
 	await exec(`${sudo} date -s "${dateFormat(dateTime, "UTC:mm/dd/yyyy HH:MM:ss")}" --utc`);
