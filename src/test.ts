29b21591642a22317ff40a4c7606b31f97e50cac
Inrix <Inrixia@gmail.com>
2022-10-05 16:43:28 +1300

    Updated tests & changed tsc target to esnext

---
+++ src/test.ts
@@ -0,0 +1,18 @@
+import DateTimeControl from ".";
+
+const timeDiff = (a: Date, b: Date) => {
+	const diff = a.getTime() - b.getTime();
+	if (diff < 0) return diff * -1;
+	return diff;
+};
+
+const dateToSet = new Date("7/8/2017 9:45:32 AM");
+const hrtime = process.hrtime();
+DateTimeControl.setDateTime(dateToSet).then(() => {
+	const [s, ns] = process.hrtime(hrtime);
+	console.log(`Setting time took: ${s * 1000 + ns / 1000000}ms`);
+	const newDate = new Date();
+	if (timeDiff(dateToSet, newDate) > 10) {
+		throw new Error(`Tests Failed! System time was not set correctely.\nExpected: ${dateToSet.toISOString()}\nGot: ${newDate.toISOString()}`);
+	}
+});
--- test.js
+++
@@ -1,5 +0,0 @@
-var control = require('./dist/index');
-
-control.setDateTime(new Date('7/8/2017 9:45:22 AM'));
-
-console.log(new Date());
